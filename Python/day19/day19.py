a985abc0a9409c60537b5e283a524caa6e21d870
SveterCZE <petr.smerkl@live.com>
2023-11-11 19:10:38 +0100

    day 19, complete

--- Python/day19/day19.py
+++ Python/day19/day19.py
@@ -12,6 +12,24 @@ def part1(instructions):
         elf_circle.remove_gifts()
     return elf_circle.get_current_turn().get_order()
 
+def part2(instructions):
+    elf_circle = CircularList()
+    for i in range(instructions):
+        elf_circle.add_new_node()
+    elf_circle.close_circle()
+    is_step_even = is_even(instructions)
+    middle_pointer_distance = instructions // 2
+    middle_pointer = elf_circle.first_inserted_node
+    for i in range(middle_pointer_distance):
+        middle_pointer = middle_pointer.next_node
+    elf_circle.taking_turn = middle_pointer.previous_node
+    while elf_circle.get_size() > 1:
+        elf_circle.remove_gifts()
+        if is_step_even == True:
+            elf_circle.taking_turn = elf_circle.taking_turn.previous_node
+        is_step_even = not is_step_even
+    return elf_circle.get_current_turn().get_order()
+
 class CircularList:
     def __init__(self):
         self.counter = 0
@@ -54,8 +72,13 @@ class CircularNode:
     def get_order(self):
         return self.order
 
+def is_even(checked_number):
+    if (checked_number % 2 == 0):
+        return True
+    else:
+        return False
 
 if __name__ == "__main__":
     instructions = get_instructions()
     print(part1(instructions))
-    # print(part2(instructions))
+    print(part2(instructions))
